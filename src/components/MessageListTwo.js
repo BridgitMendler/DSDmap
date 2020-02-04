1e1e53061cf59904f11960d440b29f3bdb29e7da
BridgitMendler <cblsunset@gmail.com>
2020-02-03 20:10:46 -0500

    diff way to get selected

--- src/components/MessageListTwo.js
+++ src/components/MessageListTwo.js
@@ -108,24 +108,15 @@ export function MessageListTwo(props){
 
 // console.log(window.event)
     const windowFocus = (val) => {
-      console.log(val)
-      var theOne = (document.getElementsByClassName('selectionssss'))
-      console.log(theOne)
-      if (typeof theOne[0] !== 'undefined'){
-        console.log('not undefined')
-        if (theOne[0].innerText === val.print) {
-          console.log('yes theOne[0].innerText ' +typeof theOne[0].innerText + ' val.print ' + typeof val.print)
-          console.log('yes theOne[0].innerText ' +theOne[0].innerText + ' val.print ' + val.print)
+        if (val.classNameParent === 'listyTwo dropTarget selectionssss') {
+          console.log('yes match '+ val.classNameParent)
+          // console.log('yes theOne[0].innerText ' +theOne[0].innerText + ' val.print ' + val.print)
           return styles.li2
         }
-        else { console.log('no theOne[0].innerText ' +theOne[0].innerText + ' val.print ' + val.print)
+        else { console.log('no match '+ val.classNameParent)
           return styles.li}
     }
-      else{
-        console.log('was undefined '+ 'theOne' +theOne + ' val.print ' + val.print)
-return styles.li
-}
-}
+
 // windowFocus('lksagl')
 //     const handleClick=() =>{
 //       // let event = e as Event;
@@ -191,9 +182,9 @@ const messageClean = () => {
     // toRemove.push(k)
   }
 }
-  console.log(newArray)
+  // console.log(newArray)
   }
-console.log(resultTwo)
+// console.log(resultTwo)
 }
 messageClean()
 
