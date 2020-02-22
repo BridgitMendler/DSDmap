c1149d8d31b2f391d26de66ac5d7af3a6600ee91
BridgitMendler <cblsunset@gmail.com>
2020-02-22 13:22:39 -0500

    switch password

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ export const Cheese = () => {
         usernameTwo['consent']= newText[2]
         usernameTwo['password'] = newText[1]
         // console.log(usernameTwo)
-        if (usernameTwo['password'] === 'Blah' && usernameTwo['consent'] === 'I consent' && usernameTwo['username'] !== ''||usernameTwo['password'] === 'Blah' && usernameTwo['consent'] === '' && usernameTwo['username'] !== ''){
+        if (usernameTwo['password'] === 'session3pm' && usernameTwo['consent'] === 'I consent' && usernameTwo['username'] !== ''||usernameTwo['password'] === 'Blah' && usernameTwo['consent'] === '' && usernameTwo['username'] !== ''){
           // console.log(usernameTwo['password'])
         setCurrentUsername(username);
         setCurrentScreen('ChatScreen');
